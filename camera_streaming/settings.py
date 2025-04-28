aae42a8ca960db202b384f0e7da65867558767b6
Rajesh Gupta <rajesh.gupta@mavenai.in>
2025-04-28 14:07:15 +0530

    Allowed all origins

--- camera_streaming/settings.py
+++ camera_streaming/settings.py
@@ -110,23 +110,23 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'camex',
-        'USER': 'postgres',
-        'PASSWORD': 'rajesh123',
-        'HOST': 'localhost',  
-        'PORT': '5432',       
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'camex',
+#         'USER': 'postgres',
+#         'PASSWORD': 'rajesh123',
+#         'HOST': 'localhost',  
+#         'PORT': '5432',       
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
