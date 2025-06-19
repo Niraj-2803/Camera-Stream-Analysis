0264c86356c4bc75d96cb8eda814c009695b3baa
Rajesh Gupta <rajesh.gupta@mavenai.in>
2025-06-19 13:24:11 +0530

    changes in mode

--- camera/routing.py
+++ camera/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/stream/<int:user_id>/<int:camera_id>/', consumers.CameraStreamConsumer.as_asgi()),
+    path('ws/stream/<int:user_id>/<int:camera_id>/<str:mode>/', consumers.CameraStreamConsumer.as_asgi()),
 ]
