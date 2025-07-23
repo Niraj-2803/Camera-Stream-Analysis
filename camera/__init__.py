eb617aaf7557423ed697cdc61f716f0fd472caae
Rajesh Gupta <rajesh.gupta@mavenai.in>
2025-07-23 17:01:09 +0530

    Added seat status for background schedule

--- camera/__init__.py
+++ camera/__init__.py
@@ -1,1 +1,6 @@
-from . import tasks
+from __future__ import absolute_import, unicode_literals
+
+# Import Celery application so it gets registered when Django starts
+from camera_streaming.celery import app as celery_app
+
+__all__ = ('celery_app',)
