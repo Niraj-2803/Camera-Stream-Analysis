2c8ce0e06bd071e7beb1606b0a5d2ce1f08bb74d
Niraj-2803 <nirajtupsundar28@gmail.com>
2025-10-24 13:00:14 +0530

    prod setup

--- camera_streaming/asgi.py
+++ camera_streaming/asgi.py
@@ -1,10 +1,14 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import camera.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camera_streaming.settings')
+
+django.setup()
+
+import camera.routing  # import AFTER django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
